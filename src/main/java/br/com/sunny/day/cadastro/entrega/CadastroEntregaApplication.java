f83ca404de3155b3bcb0fd4478119b7fbe6e9d1e
Michel Soliva <michels@ciandt.com>
2021-04-28 16:50:49 -0300

    Criação Projeto

---
+++ src/main/java/br/com/sunny/day/cadastro/entrega/CadastroEntregaApplication.java
@@ -0,0 +1,13 @@
+package br.com.sunny.day.cadastro.entrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroEntregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroEntregaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sunny/day/cadastro/entrega/CadastroEntregaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sunny.day.cadastro.entrega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroEntregaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
